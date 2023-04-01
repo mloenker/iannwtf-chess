2beef5ce4fe5a10368fa0b96d4eab803035138ce
Silvie Opolka <117082815+silvieopolka@users.noreply.github.com>
2023-04-01 09:10:48 +0200

    Update read_games.py

--- read_games.py
+++ read_games.py
@@ -1,6 +1,15 @@
 import re
 
 def read_games(raw_file_path, number_of_games, format="pgn", write_to_file=True, target_file_path="games.txt", startpoint=0):
+    """
+        Reads games frome the PGN file
+            - raw_file_path = path to the PGN file (e.g. stored in GoogleDrive)
+            - number_of_games = amount of games to get from the file
+            - format = notation used (either "pgn" or "san"/"pgn")
+            - write_to_file = writes the extracted games to a file (if True) or returns the extracted games (if False)
+            - target_file_path = path of file the extracted games get written to
+            - startpoint = index after which game of file to start extracting games
+    """
     number_of_games = number_of_games + startpoint
     with open(raw_file_path, 'r') as file:
         games = []
